32e4aa785add3ff2765c226f83303cc52e96365d
OussamaELAZHARI <oussama1elazhari@gmail.com>
2015-08-12 15:06:28 +0200

    Update saxParse.R

--- saxParse.R
+++ saxParse.R
@@ -1,39 +1,42 @@
-library("XML")
+# assign("is.title",T,.GlobalEnv,inherits =T)
+# assign("title","",.GlobalEnv,inherits =T)
+# assign("output",data.frame(title=character()),.GlobalEnv,inherits =T)
 
-parse_xml_dump <- function (input, output) {
-  data1 = xmlParse (input)
-  gen_frame <- function(n) {
-    # getting the fields
-    title = xmlValue(xmlElementsByTagName(xmlParent(n),"title")[[1]])
-    timestamp = xmlValue(xmlElementsByTagName(n,"timestamp")[[1]])
-    
-    if(length(xmlElementsByTagName(n,"contributor"))>0){
-      contributor = xmlElementsByTagName(n,"contributor")[[1]]
-      
-      if(nchar(xmlValue(contributor))>0){
-        if (length(xmlElementsByTagName(contributor,"username"))==0){
-          contributorRef = xmlValue(xmlElementsByTagName(contributor,"ip")[[1]]) 
-        }
-        if (length(xmlElementsByTagName(contributor,"ip"))==0){
-          contributorRef = xmlValue(xmlElementsByTagName(contributor,"username")[[1]]) 
-        }
-      }
-      else{  
-        contributorRef = NA 
-      }
-    }
-    else {
-      contributorRef = NA 
-    }
-    f<-data.frame(title=title, timestamp =timestamp , contributor = contributorRef)
-    print(f)
-    
-  }
-  
-  f <- Reduce(rbind,xpathApply(data1, "//x:revision",fun=gen_frame,namespaces=c(x="http://www.mediawiki.org/xml/export-0.10/")))
-  
-  write.table(f, file = output, append = FALSE, quote = TRUE, sep = "^",eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = TRUE, qmethod = c("escape", "double"),fileEncoding = "")
+startElmt = function(name, attr,...) {
+  if(name=="title"){is.title<<-T}
 }
 
-parse_xml_dump ("enwikivoyage-20150702-stub-meta-history.xml.gz", "parse_enwikivoyage-20150702-stub-meta-history.csv")
-#parse_xml_dump ("extract.xml", "parse_extract.csv")
+endElmt = function(name, attr,...) {
+  if(name=="title"){is.title<<-F}
+  if(name=="revision"){
+    print ("revision")
+    output <- rbind(output,data.frame(title=title))
+    #print(paste("output size",length(output)))
+    #print(paste("output title",title))
+    
+  }
+}
+
+getText = function(content,...){
+  # unlockBinding("title", .GlobalEnv)
+  if(is.title<<-T){ title <<- title }
+}
+
+
+env = new.env()
+environment(startElmt) = env
+environment(endElmt) = env
+environment(getText) = env
+env$pages = pages
+env$n = 0
+ 
+assign("is.title",F,pos=env)
+assign("title","",pos=env)
+assign("output",data.frame(title=character()),pos=env)
+
+#environment(startElmt) = .GlobalEnv
+#environment(endElmt) = .GlobalEnv
+#environment(getText) = .GlobalEnv
+
+
+z <- xmlEventParse("extract.xml", handlers = list(startElement=startElmt, endElement=endElmt, text=getText))
